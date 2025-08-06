39edcb8001618c702f9ae508b8cae4ba1df07689
Gustavo Leck <gustavoleck@hotmail.com>
2025-08-06 15:24:20 -0300

    fix(Corrige liberação de portas)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cloudflared access tcp --hostname teste8.leck.com.br --url 0.0.0.0:5008 &
+cloudflared access tcp --hostname teste10.leck.com.br --url 0.0.0.0:5010 &
 cloudflared access tcp --hostname teste20.leck.com.br --url 0.0.0.0:5020 &
 
 wait
